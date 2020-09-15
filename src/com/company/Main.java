ccf4af66413879f076d5b4b36626c0f310901da4
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-09-15 16:36:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String input = keyboard.nextLine();
+        System.out.println("Hello, " + input);
+
+
+
+    }
+}
